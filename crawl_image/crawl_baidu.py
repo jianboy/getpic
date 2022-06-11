9a1eaafa6f7f58643cec6e6849a7e324d9d0c715
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2022-06-11 17:21:15 +0800

    muti thread to download

--- crawl_image/crawl_baidu.py
+++ crawl_image/crawl_baidu.py
@@ -6,10 +6,15 @@
 @License :   Copyright © 2017-2022 liuyuqi. All Rights Reserved.
 @Desc    :   baidu image search
 '''
+from concurrent.futures import ThreadPoolExecutor
+from contextlib import closing
 import re
+from socket import timeout
 from crawl_image.crawl_image import CrawlImage
 from crawl_image import api
 import os
+
+from crawl_image.libs.download_progress import DownloadProgress
 
 
 class CrawlImageFromBaidu(CrawlImage):
@@ -18,9 +23,9 @@ class CrawlImageFromBaidu(CrawlImage):
 
     def run(self):
         url_init = api.baidu_search_image + self.keyword
-        all_pic_urls = []
+        allPicUrls = []
         page_urls, next_page_url = self.getPicList(url_init)
-        all_pic_urls.extend(page_urls)
+        allPicUrls.extend(page_urls)
         os.makedirs(self.savedir, exist_ok=True)
         page_count = 0  # 累计翻页数
 
@@ -32,49 +37,55 @@ class CrawlImageFromBaidu(CrawlImage):
             if next_page_url == '' and page_urls == []:
                 print('已到最后一页，共计%s个翻页' % page_count)
                 break
-            all_pic_urls.extend(page_urls)
-            if len(all_pic_urls) >= self.max_download_images:
+            allPicUrls.extend(page_urls)
+            if len(allPicUrls) >= self.max_download_images:
                 print('已达到设置的最大下载数量%s' % self.max_download_images)
                 break
 
-        self.downPic(list(set(all_pic_urls)))
+        self.downloadPic(list(set(allPicUrls)))
 
-    def getPicList(self, page_url):
-        if not page_url:
+    def getPicList(self, pageUrl):
+        if not pageUrl:
             return [], ''
         try:
-            html = self.sess.get(page_url)
+            html = self.sess.get(pageUrl)
             html.encoding = 'utf-8'
             html = html.text
         except IOError as e:
             return [], ''
         pic_urls = re.findall('"objURL":"(.*?)",', html, re.S)
-        print(pic_urls)
-        next_page_url = re.findall(re.compile(
-            r'<a href="(.*)" class="n">下一页</a>'), html, flags=0)
+        next_page_url = re.findall(re.compile(r'<a href="(.*)" class="n">下一页</a>'), html, flags=0)
         next_page_url = 'http://image.baidu.com' + \
             next_page_url[0] if next_page_url else ''
         return pic_urls, next_page_url
 
-    def downPIc(self, pic: str):
+    def downloadPic(self, picUrl: str,fileName: str):
         '''
-        download a picture'''
-        try:
-            res=self.sess.get(pic,timeout=5)
-            
-        pass
+        download a picture
+        '''
+        with closing(self.sess.get(url=picUrl, stream=True, timeout=10)) as response:
+            chunkSize = 1024
+            contentSize = int(response.headers["content-length"])
+            if(os.path.exists(fileName) and os.path.getsize(fileName) == contentSize):
+                print("跳过" + fileName)
+            else:
+                progress = DownloadProgress.DownloadProgress(fileName, total=contentSize, unit="KB",
+                                                             chunk_size=chunkSize, run_status="downloading", fin_status="downloaded")
+                if not os.path.exists(os.path.dirname(fileName)):
+                    os.makedirs(os.path.dirname(fileName))
+                with open(fileName, "wb") as file:
+                    for data in response.iter_content(chunk_size=chunkSize):
+                        file.write(data)
+                        progress.refresh(count=len(data))
 
-    def downPic(self, pic_urls: list):
-        pic_urls = pic_urls[:self.max_download_images]
-        for i, pic_url in enumerate(pic_urls):
+    def downloadPictures(self, picurls: list):
+        picurls = picurls[:self.max_download_images]
+        pool = ThreadPoolExecutor(max_workers=10)
+        for i, picUrl in enumerate(picurls):
             try:
-                pic = self.sess.get(pic_url, timeout=15)
-                imgpath = os.path.join(
-                    self.savedir, "baidu_fgjianzhu_{}.jpg".format(i+1))
-                with open(imgpath, "wb") as f:
-                    f.write(pic.content)
-                    print('成功下载第%s张图片: %s' % (str(i + 1), str(pic_url)))
+                pool.submit(self.downloadPic, picUrl, self.savedir +r"/baidu{}.jpg".format(i+1))
+                print('成功下载第%s张图片: %s' % (str(i + 1), str(picUrl)))
             except IOError as e:
-                print('下载第%s张图片时失败: %s' % (str(i + 1), str(pic_url)))
+                print('下载第%s张图片时失败: %s' % (str(i + 1), str(picUrl)))
                 print(e)
                 continue
